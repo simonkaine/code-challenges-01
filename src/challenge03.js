58a53edc9589f6934c32a6ef6fa6118b8f030e63
Simon Kaine <simonkaine@gmail.com>
2021-08-10 16:11:17 -0700

    prob-03

--- src/challenge03.js
+++ src/challenge03.js
@@ -1,3 +1,7 @@
 export function capitalizeNames(arr){
-  return arr.map(string => string.charAt(0).toUpperCase() + string.slice())
+  return arr.map(string => {
+    const firstWord = string.toLowerCase()
+    const fixedWord = firstWord.charAt(0).toUpperCase() + firstWord.slice(1);
+    return fixedWord;
+  })
 }
