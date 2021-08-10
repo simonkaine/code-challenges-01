72c0be67dede804dadb1c35020501c71f39e85c8
Simon Kaine <simonkaine@gmail.com>
2021-08-10 10:11:50 -0700

    prob-01

---
+++ src/challenge01.js
@@ -0,0 +1,3 @@
+export function doubleNumbers(arr) {
+    return arr.map(number => number * 2)
+}
---
+++ src/challengeTest01.test.js
@@ -0,0 +1,7 @@
+import { doubleNumbers } from './challenge01';
+
+test('doubles numbers', () => {
+  const input = [2, 5, 100]; // arrange
+  const output = doubleNumbers(input); // act
+  expect(output).toEqual([4, 10, 200]); // assert
+});
