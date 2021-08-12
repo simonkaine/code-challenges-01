876f157c261e606cb9632537f05bc008858620f7
Simon Kaine <simonkaine@gmail.com>
2021-08-11 17:38:43 -0700

    codeChallengeLab02

---
+++ src/lab02Challenge01.js
@@ -0,0 +1,7 @@
+export function fiveAndGreaterOnly(arr) {
+    return arr.filter((num) => {
+        if (num >= 5) {
+            return num;
+        }
+    })
+}
---
+++ src/lab02challenges.test.js
@@ -0,0 +1,7 @@
+import { fiveAndGreaterOnly } from './lab02Challenge01';
+
+test('Five and greater only', () => {
+  const input = [3, 6, 8, 2]; // arrange
+  const output = fiveAndGreaterOnly(input); // act
+  expect(output).toEqual([6, 8]); // assert
+});
