c5229b6fcedb980322828bfa10be3e85126cc37c
Simon Kaine <simonkaine@gmail.com>
2021-08-17 20:44:15 -0700

    get Houses + update Numbers (prob 01 & prob 02).

---
+++ src/more-object-keys-entries.js
@@ -0,0 +1,8 @@
+export function getHouses(obj) {
+    return Object.values(obj).map(obj => obj.house)
+}
+
+export function updateNumbers(obj) {
+    return Object.entries(obj).map(obj => obj[0] + ": " + obj[1])
+}
+
