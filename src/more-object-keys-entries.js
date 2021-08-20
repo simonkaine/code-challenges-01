f62232c14546ae4a5c0c6e300ab889ed5bc1ba37
Simon Kaine <simonkaine@gmail.com>
2021-08-20 09:44:32 -0700

    House Size

--- src/more-object-keys-entries.js
+++ src/more-object-keys-entries.js
@@ -6,3 +6,14 @@ export function updateNumbers(obj) {
     return Object.entries(obj).map(obj => obj[0] + ": " + obj[1])
 }
 
+export function totalCharacters(arr) {
+    let count = 0;
+    let obj = Object.values(arr);
+    
+    obj.forEach(arr => { 
+        count += arr.children.length;
+        if (arr.spouse) count++;
+        count++
+    })
+    return count;
+}
--- src/more-object-keys-entries.test.js
+++ src/more-object-keys-entries.test.js
@@ -1,4 +1,4 @@
-import { getHouses, updateNumbers } from './more-object-keys-entries.js';
+import { getHouses, totalCharacters, updateNumbers } from './more-object-keys-entries.js';
 
 test('it gets the names of the houses', () => {
   const characters = [
@@ -53,4 +53,53 @@ test('Update Numbers', () => {
   const characters = {'Grace Hopper': '222-303-5938', 'Ada Lovelace': '222-349-9842', 'Alan Turing': '222-853-5933'}; //arrange
   const output = updateNumbers(characters); //act
   expect(output).toEqual(['Grace Hopper: 222-303-5938','Ada Lovelace: 222-349-9842','Alan Turing: 222-853-5933']); //assert
+});
+
+test('House Size', () => {
+  const characters = [
+    {
+      name: 'Eddard',
+      spouse: 'Catelyn',
+      children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+      house: 'Stark'
+    },
+    {
+      name: 'Jon A.',
+      spouse: 'Lysa',
+      children: ['Robin'],
+      house: 'Arryn'
+    },
+    {
+      name: 'Cersei',
+      spouse: 'Robert',
+      children: ['Joffrey', 'Myrcella', 'Tommen'],
+      house: 'Lannister'
+    },
+    {
+      name: 'Daenarys',
+      spouse: 'Khal Drogo',
+      children: ['Drogon', 'Rhaegal', 'Viserion'],
+      house: 'Targaryen'
+    },
+    {
+      name: 'Mace',
+      spouse: 'Alerie',
+      children: ['Margaery', 'Loras'],
+      house: 'Tyrell'
+    },
+    {
+      name: 'Euron',
+      spouse: null,
+      children: [],
+      house: 'Greyjoy'
+    },
+    {
+      name: 'Jon S.',
+      spouse: null,
+      children: [],
+      house: 'Snow'
+    }
+  ]; //arrange; //arrange
+  const output = totalCharacters(characters); //act
+  expect(output).toEqual(26); //assert
 });
