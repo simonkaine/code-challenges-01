ca0b24d76f7541945f5dbfc1d6c2fe1037327ae3
Simon Kaine <simonkaine@gmail.com>
2021-08-26 15:35:21 -0700

    Find Max

--- src/code-challenge-11.test.js
+++ src/code-challenge-11.test.js
@@ -1,6 +1,10 @@
-import { returnTen } from './codeChallenge11';
+import { returnTen, findMax } from './codeChallenge11';
 
   test('it should return the last 10 characters of a string as an array', () => {
     expect(returnTen('hello world')).toStrictEqual(['e','l','l','o',' ','w','o','r','l','d']);
     expect(returnTen('world')).toStrictEqual(['w','o','r','l','d']);
+  });
+
+  test('Find max number', () => {
+    expect(findMax([[13,24,24,2], [2,5,6], [2,3]])).toEqual(24);
   });
--- src/codeChallenge11.js
+++ src/codeChallenge11.js
@@ -1,3 +1,9 @@
 export function returnTen(str) {
     return str.split('').slice(-10);
 }
+
+export function findMax(matrix) {
+    const maxNum = matrix;
+    const max = Math.max(...[].concat(...maxNum));
+    return max;
+}
