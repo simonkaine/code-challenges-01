a8f10c20289519590c9a4f0aa8071c230e82020b
Simon Kaine <simonkaine@gmail.com>
2021-08-26 15:59:15 -0700

    Total Sum

--- src/code-challenge-11.test.js
+++ src/code-challenge-11.test.js
@@ -1,4 +1,4 @@
-import { returnTen, findMax } from './codeChallenge11';
+import { returnTen, findMax, totalSum } from './codeChallenge11';
 
   test('it should return the last 10 characters of a string as an array', () => {
     expect(returnTen('hello world')).toStrictEqual(['e','l','l','o',' ','w','o','r','l','d']);
@@ -7,4 +7,9 @@ import { returnTen, findMax } from './codeChallenge11';
 
   test('Find max number', () => {
     expect(findMax([[13,24,24,2], [2,5,6], [2,3]])).toEqual(24);
+  });
+
+  test('Find the Total Sum', () => {
+    expect(totalSum([[13,24,24,2], [2,5,6], [2,3]])).toEqual(81);
+    expect(totalSum([])).toEqual(0);
   });
--- src/codeChallenge11.js
+++ src/codeChallenge11.js
@@ -7,3 +7,16 @@ export function findMax(matrix) {
     const max = Math.max(...[].concat(...maxNum));
     return max;
 }
+
+export function totalSum(matrix) {
+    let sum = 0;
+    matrix.forEach((item) => {
+      if(Array.isArray(item)) {
+       sum += totalSum(item);
+      } else {
+      sum += item;
+      }
+    })
+    return sum;
+}
+
