6ef76631c7b9990763496888a2662c0577c53ee3
Simon Kaine <simonkaine@gmail.com>
2021-08-26 16:27:58 -0700

    Store hours open

--- src/code-challenge-11.test.js
+++ src/code-challenge-11.test.js
@@ -1,4 +1,4 @@
-import { returnTen, findMax, totalSum } from './codeChallenge11';
+import { returnTen, findMax, totalSum, grandTotal } from './codeChallenge11';
 
   test('it should return the last 10 characters of a string as an array', () => {
     expect(returnTen('hello world')).toStrictEqual(['e','l','l','o',' ','w','o','r','l','d']);
@@ -13,3 +13,9 @@ import { returnTen, findMax, totalSum } from './codeChallenge11';
     expect(totalSum([[13,24,24,2], [2,5,6], [2,3]])).toEqual(81);
     expect(totalSum([])).toEqual(0);
   });
+
+//   test('grand total', () => {
+
+//     expect(grandTotal(cookieStores)).toEqual([88, 153, 252, 286, 139, 161, 145, 232, 276, 207, 161, 169]);
+    
+//   });
