f2431302aeab7977e5856b8d3be56abd9ba6a1c0
Simon Kaine <simonkaine@gmail.com>
2021-08-26 19:15:42 -0700

    prob 01 - prob 02 UNSOLVED. fixed red x?

--- src/code-challenge-13.js
+++ src/code-challenge-13.js
@@ -1,4 +1,4 @@
-const sortByChildren = (charArray) => {
+// const sortByChildren = (charArray) => {
     // I gave this one a good hour-ish but have yet to come up with solution
     // I think the test threw me off here. But Im guessing i would 
     // have to for loop the char array and run an if condition that 
@@ -6,13 +6,13 @@ const sortByChildren = (charArray) => {
     // I wasn't sure how to sort alphabetically by house name after if same 
     // number of children existed though
    
-};
+// };
 
-const containsW = (str) => {
+// const containsW = (str) => {
     // I felt I almost had this one and am hoping to return to it on break
     // after project week because I feel i def know how to do this one.
     // I need to get better at reading this function first
     // but i think I need just search using charAt() and includes() for this one.
-};
+// };
 
 // I am hoping to return to these during break but I wasn't able to do these 6.
